62ca62184301a17309e36dc8d349560006e4991f
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-23 12:06:14 -0800

    factorialTrailingZeroes implemented

--- problems/factorial_trailing_zeroes/factorial_trailing_zeroes.hpp
+++ problems/factorial_trailing_zeroes/factorial_trailing_zeroes.hpp
@@ -1,7 +1,10 @@
-namespace problem {
-    unsigned int factorialTrailingZeroes(unsigned long int){
+namespace problems {
+    unsigned int factorialTrailingZeroes(unsigned long int operand){
         unsigned int zeroCount = 0;
-
+        while (operand > 0){
+            operand /= 5;
+            zeroCount += operand;
+        }
         return zeroCount;
     }
 }
