8f66d597d1a36293d848bf6868023219b0fb51e3
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-23 12:05:23 -0800

    finished factorialTrailingZeroes setup

--- problems/factorial_trailing_zeroes/main.cpp
+++ problems/factorial_trailing_zeroes/main.cpp
@@ -1,17 +1,14 @@
-#include <iostream>
 #include "./factorial_trailing_zeroes.hpp"
 #include "../../tester/tester.hpp"
 
 int main(){
-    const auto testName = "factorialTrailingZeroes(50)";
+    char testName[] = "factorialTrailingZeroes(50)";
 
     unsigned int
-        expected = 8,
-        actual = problem::factorialTrailingZeroes(50);
+        expected = 12,
+        actual = problems::factorialTrailingZeroes(50);
 
-    std::cout << testName << std::endl;
-    std::cout << "Expected: " << expected << std::endl;
-    std::cout << "Actual: " << actual << std::endl;
+    tester::assert(testName, expected, actual);
 
     return 0;
 }
--- tester/tester.hpp
+++ tester/tester.hpp
@@ -6,7 +6,8 @@ namespace tester {
             std::cout << "PASSED - " << testName << " = " << expected << std::endl;
             return true;
         } else {
-            std::cout << "FAILED - " << testName << " = " << expected << std::endl;
+            std::cout << "FAILED - " << testName << ":" << std::endl;
+            std::cout << "Expected output: " << expected << std::endl;
             std::cout << "Actual output: " << actual << std::endl;
             return false;
         }
